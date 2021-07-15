429bb7cf746fba71c9866189e4f9ae65e840af29
Sebastian Olaru <sebastian.olaru@rocjmac009.local>
2021-07-15 23:31:08 +0300

    checker and remove functionality

--- App.js
+++ App.js
@@ -28,6 +28,8 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 const vw = Dimensions.get('window').width / 100;
 const vh = Dimensions.get('window').height / 100;
 
@@ -64,20 +66,43 @@ const App = () => {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState([
+    { title: 'Servus', checked: false },
+    { title: 'Do the UI', checked: true },
+    { title: 'Refactorize', checked: false },
+  ]);
 
   const [currentTask, setCurrentTask] = useState('');
 
   const [isEditing, setIsEditing] = useState(false);
 
   const handleTaskAdd = () => {
-    setTasks([...tasks, currentTask]);
-    setCurrentTask('');
+    let tempTask = {
+      title: currentTask,
+      checked: false,
+    };
+
+    if (tempTask.checked !== '') {
+      setTasks([...tasks, tempTask]);
+      setCurrentTask('');
+    }
   };
 
-  const handleRemove = () => {};
+  const handleRemove = id => {
+    setTasks(tasks.filter((value, index, arr) => index !== id));
+  };
   const handleEditing = () => {
     setIsEditing(!isEditing);
+  };
+  const handleCheck = id => {
+    let updatedTasks = [];
+    tasks.forEach((task, index) => {
+      updatedTasks.push({
+        title: task.title,
+        checked: index == id ? !task.checked : task.checked,
+      });
+    });
+    setTasks(updatedTasks);
   };
 
   return (
@@ -85,15 +110,43 @@ const App = () => {
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <View style={styles.titleRegion}>
         <Text style={styles.sectionTitle}>ToDoor</Text>
-        <Button title="x" onPress={handleEditing} />
+        <Icon
+          name="edit"
+          onPress={handleEditing}
+          style={styles.editButton}
+          size={26}
+        />
       </View>
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={[backgroundStyle]}>
         {tasks.map((task, id) => (
-          <View key={id}>
-            <Text style={[styles.inputField]}>{task}</Text>
-            {isEditing && <Button title="x" />}
+          <View key={id} style={[styles.listElement]}>
+            {!isEditing && (
+              <Icon
+                name={task.checked === true ? 'check-square' : 'square-o'}
+                onPress={() => handleCheck(id)}
+                style={styles.listElementButton}
+                size={18}
+              />
+            )}
+            <Text style={[styles.listElementText]}>{task.title}</Text>
+            {isEditing && (
+              <Icon
+                name="close"
+                onPress={() => handleRemove(id)}
+                style={styles.listElementButton}
+                size={18}
+              />
+            )}
+            {/* {!isEditing ? (
+              <Text style={[styles.inputField]}>{task}</Text>
+            ) : (
+              <TextInput
+                style={[styles.inputField]}>
+                {task}
+              </TextInput>
+            )} */}
           </View>
         ))}
 
@@ -119,7 +172,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 24,
   },
   sectionTitle: {
-    color: Colors.dark,
+    color: Colors.darker,
     fontSize: 32,
     fontWeight: '800',
     margin: 2 * vh,
@@ -133,18 +186,48 @@ const styles = StyleSheet.create({
     fontWeight: '700',
   },
   inputField: {
-    borderRadius: 2 * vh,
-    color: Colors.dark,
     fontWeight: '700',
     fontSize: 18,
+    borderRadius: 2 * vh,
+    color: Colors.darker,
     borderWidth: 3,
     padding: 3 * vw,
     height: 6 * vh,
     margin: 10,
   },
+  listElement: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderRadius: 2 * vh,
+    borderWidth: 3,
+    padding: 3 * vw,
+    margin: 10,
+  },
+  listElementText: {
+    fontWeight: '700',
+    fontSize: 18,
+    color: Colors.darker,
+    alignSelf: 'center',
+  },
+  listElementButton: {
+    display: 'flex',
+    paddingLeft: 0.3,
+    backgroundColor: '#0000',
+    marginRight: 0,
+    alignSelf: 'center',
+  },
+  editButton: {
+    display: 'flex',
+    paddingLeft: 0.3,
+    backgroundColor: '#0000',
+    marginRight: 3 * vw,
+    alignSelf: 'center',
+  },
   titleRegion: {
     display: 'flex',
     flexDirection: 'row',
+    justifyContent: 'space-between',
   },
 });
 
